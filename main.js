7001c155d00710b6a14b08e44f6744641ed8bd01
NickChunglolz <zxc87523@gmail.com>
2017-12-18 20:20:52 +0800

    cool

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter Earth"";
-=======
-var truth = "Hello Flat Earth";
->>>>>>> 0b7e3deadb5739d6ece6fd4c3d346169cb941848
+var truth = "Hello Flat Earth"";
+
